e6a27f58041abc7d676055bcab65f584b1517c88
Pavel Sterhov <pasha.sterhow@yandex.ru>
2024-10-15 17:21:55 +0300

    15_2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
+  width: 245,
+  spaceBetween: 20,
+  grabCursor: true,
+
   pagination: {
       el: ".swiper-pagination",
+      clickable: true,
   },
 });
